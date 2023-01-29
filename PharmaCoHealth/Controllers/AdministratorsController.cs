7976a42534970c8741853f681544d4826b27422d
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2023-01-29 11:02:42 +0600

    First Commit

---
+++ PharmaCoHealth/Controllers/AdministratorsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PharmaCoHealth.Models;
+using PharmaCoHealth.ViewModel;
+
+namespace PharmaCoHealth.Controllers
+{
+    public class AdministratorsController : Controller
+    {
+        private PharmaCoHealth_25Entities db = new PharmaCoHealth_25Entities();
+
+        // GET: Administrators
+        public ActionResult Index()
+        {
+            return View(db.Administrators.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(AdminLogInViewModel admin)
+        {
+            if (ModelState.IsValid)
+            {
+                var admn = db.Administrators.Where(a => a.Email.Equals(admin.Email) && a.Password.Equals(admin.Password)).FirstOrDefault();
+                if (admn == null)
+                {
+                    ViewBag.NoAdmin = "No Admin Found";
+                    return View();
+                }
+                else
+                {
+                    return Content("Log in Successful");
+                }
+            }
+            return View();
+        }
+
+        // GET: Administrators/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Administrator administrator = db.Administrators.Find(id);
+            if (administrator == null)
+            {
+                return HttpNotFound();
+            }
+            return View(administrator);
+        }
+
+        // GET: Administrators/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Administrators/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AdminId,AdminFirstName,AdminLastName,AdminPhoneNo,Email,Password,ConfirmPassword")] Administrator administrator)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Administrators.Add(administrator);
+                db.SaveChanges();
+                return RedirectToAction("LogIn");
+            }
+
+            return View(administrator);
+        }
+
+        // GET: Administrators/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Administrator administrator = db.Administrators.Find(id);
+            if (administrator == null)
+            {
+                return HttpNotFound();
+            }
+            return View(administrator);
+        }
+
+        // POST: Administrators/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AdminId,AdminFirstName,AdminLastName,AdminPhoneNo,Email,Password")] Administrator administrator)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(administrator).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(administrator);
+        }
+
+        // GET: Administrators/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Administrator administrator = db.Administrators.Find(id);
+            if (administrator == null)
+            {
+                return HttpNotFound();
+            }
+            return View(administrator);
+        }
+
+        // POST: Administrators/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Administrator administrator = db.Administrators.Find(id);
+            db.Administrators.Remove(administrator);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PharmaCoHealth/Controllers/PatientsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PharmaCoHealth.Models;
+using PharmaCoHealth.ViewModel;
+
+namespace PharmaCoHealth.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private PharmaCoHealth_25Entities db = new PharmaCoHealth_25Entities();
+
+        // GET: Patients
+        public ActionResult Index()
+        {
+            return View(db.Patients.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(PatientLogInViewModel patient)
+        {
+            if (ModelState.IsValid)
+            {
+                var pat = db.Patients.Where(a => a.Email.Equals(patient.Email) && a.Password.Equals(patient.Password)).FirstOrDefault();
+                if (pat == null)
+                {
+                    ViewBag.NoPatient = "No Patients Found";
+                    return View();
+                }
+                else
+                {
+                    return Content("Log in Successful");
+                }
+            }
+            return View();
+        }
+
+        // GET: Patients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // GET: Patients/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PatientId,PatientFirstName,PatientLastName,PatientPhoneNo,Email,Password,ConfirmPassword")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Patients.Add(patient);
+                db.SaveChanges();
+                return RedirectToAction("LogIn");
+            }
+
+            return View(patient);
+        }
+
+        // GET: Patients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PatientId,PatientFirstName,PatientLastName,PatientPhoneNo,Email,Password")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(patient);
+        }
+
+        // GET: Patients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Patient patient = db.Patients.Find(id);
+            db.Patients.Remove(patient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
